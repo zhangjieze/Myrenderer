6a02eb832dc4ac6d6e69f7fb3573b50eb41ca269
Zhang Ze <zj1491167926@163.com>
2026-06-25 18:22:56 +0800

    6.25对dda算法各个方向以及反向进一步检测

--- test.cpp
+++ test.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-#include "tgaimage.h"
 #include "rasterizer.h"
+#include "tgaimage.h"
 
 int main(){
+    TGAImage image(100,100);
     TGAColor red{0,0,255};
-    TGAColor green{0,255,0};
     TGAColor blue{255,0,0};
+    TGAColor green{0,255,0};
 
-    TGAImage image(100, 100);
-    image.set(50, 50, red);
-    image.set(60, 60, green);
-    image.set(70, 70, blue);
-    line(0, 0, 100, 100, image, red);
-    image.write("day8line.tga");
-    
-    std::cout << "success" << std::endl;
+    line(10, 10, 90, 10, image, red);    // 水平
+    line(10, 10, 10, 90, image, green);  // 垂直
+    line(10, 10, 90, 60, image, blue);   // 低斜率
+    line(10, 10, 40, 90, image, red);    // 高斜率
+    line(90, 90, 10, 20, image, green);  // 反向
+    line(50, 50, 50, 50, image, blue);   // 单点
+
+    image.write("ddatest.tga");
     return 0;
 }
