1692a3373945ad4d6b21a757c073725c206c7c8d
Zhang Ze <zj1491167926@163.com>
2026-06-26 22:05:49 +0800

    6.26完善bresenham，解决负斜率,大斜率和x0<x1反向问题

--- rasterizer.cpp
+++ rasterizer.cpp
@@ -21,16 +21,39 @@ void line_dda(int x0, int y0, int x1, int y1,TGAImage& image, const TGAColor& co
 
 //bresenham
 void line_bresenham(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color){
+    //处理斜率过大,也就是dx < dy,此时取点应该由y轴取,交换坐标轴
+    bool steep = std::abs(y1 - y0) > std::abs(x1 - x0);
+    if (steep){
+        std::swap(x0,y0);
+        std::swap(x1,y1);
+    }
+
+    //处理反向问题
+    if (x0 > x1){
+        std::swap(x0,x1);
+        std::swap(y0,y1);
+    }
     int dx = x1 - x0;
     int dy = y1 - y0;
+
+
+    //处理负斜率问题,负斜率的时候相当于y往下走,用一个ystep保存y需要走的间隔
+    int ystep = 1;
+    if (dy < 0){
+        dy *= -1;
+        ystep = -1;
+    }
+
+
     int error = 0;
     int y = y0;
     for (int x = x0 ; x <= x1;++x){
-        image.set(x,y,color);
+        if (steep) image.set(y,x,color);
+        else image.set(x,y,color);
 
         error += dy;
         if (2 * error >= dx){
-            y++;
+            y += ystep;
             error -= dx;
         }
     }
--- test.cpp
+++ test.cpp
@@ -7,9 +7,14 @@ int main(){
     TGAColor red{0,0,255};
     TGAColor blue{255,0,0};
     TGAColor green{0,255,0};
-
-    line_bresenham(10,10,90,60,image,red);
-
-    image.write("bresenham1.tga");
+    line_bresenham(10, 10, 90, 10, image, red);    // 水平
+    line_bresenham(10, 10, 10, 90, image, green);  // 垂直
+    line_bresenham(10, 10, 90, 60, image, blue);   // 低斜率正
+    line_bresenham(10, 60, 90, 10, image, red);    // 低斜率负
+    line_bresenham(10, 10, 40, 90, image, green);  // 高斜率正
+    line_bresenham(40, 90, 10, 10, image, blue);   // 高斜率反向
+    line_bresenham(90, 90, 10, 20, image, red);    // 反向混合
+    line_bresenham(50, 50, 50, 50, image, blue);   // 单点
+    image.write("bresenham_final_test.tga");
     return 0;
 }
